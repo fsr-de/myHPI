ee31d2dbe19e5fac9fc75c694af7da5e5751f766
Julian B <jeriox@users.noreply.github.com>
2021-04-10 10:15:13 +0200

    fix menu error

--- core/context.py
+++ core/context.py
@@ -2,7 +2,7 @@ from wagtail.core.models import Page, Site
 
 
 def base_context(request):
-    root_page = getattr(Site.find_for_request(request), "root_page")
+    root_page = getattr(Site.find_for_request(request), "root_page", None)
     return {
         "root_page": root_page,
         "all_pages": Page.objects.in_menu().child_of(root_page),
