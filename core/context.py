e446e7004dcfa98952518b2381f1e4d9a2715f32
Julian B <jeriox@users.noreply.github.com>
2021-04-10 10:15:48 +0200

    fix menu error

--- core/context.py
+++ core/context.py
@@ -5,5 +5,5 @@ def base_context(request):
     root_page = getattr(Site.find_for_request(request), "root_page", None)
     return {
         "root_page": root_page,
-        "all_pages": Page.objects.in_menu().child_of(root_page),
+        "all_pages": Page.objects.in_menu().child_of(root_page) if root_page else None,
     }
