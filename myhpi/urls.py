f2bad52f507c88822b29860b502cd5f05302d254
frcroth <frcroth@gmail.com>
2022-05-01 16:38:00 +0200

    Fix linting issues

--- myhpi/urls.py
+++ myhpi/urls.py
@@ -1,12 +1,11 @@
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
-
-from django.contrib.auth import views as auth_views
 
 from myhpi.search import views as search_views
 
@@ -15,10 +14,14 @@ urlpatterns = [
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("oidc/", include("mozilla_django_oidc.urls")),
-    path("login/", auth_views.LoginView.as_view(
-        template_name="login.html",
-        redirect_authenticated_user=True,
-    ), name="login"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="login.html",
+            redirect_authenticated_user=True,
+        ),
+        name="login",
+    ),
 ]
 
 
