40d5f32c9e7506aac462203f4268f6469ad90874
DrEGZo <konacom@web.de>
2022-05-16 17:27:18 +0200

    Add IP range group to everyone
    removed debug print statement
    
    Reformat the code

--- myhpi/core/context.py
+++ myhpi/core/context.py
@@ -1,6 +1,6 @@
-from wagtail.core.models import Page, Site
-from django.db.models import Q
 from django.contrib.auth.models import Group
+from django.db.models import Q
+from wagtail.core.models import Page, Site
 
 
 def base_context(request):
@@ -12,18 +12,17 @@ def base_context(request):
         page_list = []
         for page in pages:
             subpages = page.get_children().in_menu().specific()
-            if len(subpages) == 0: # no subpages - add to list if visible
+            if len(subpages) == 0:  # no subpages - add to list if visible
                 matches_group = any(group in page.visible_for.all() for group in usergroups)
-                print(page, usergroups, matches_group, page.is_public)
                 if matches_group or page.is_public:
                     page_list.append(page)
-            else: # check subpages - add only if at least 1 subpage visible
+            else:  # check subpages - add only if at least 1 subpage visible
                 visible_subpages = can_view(subpages, usergroups)
                 if len(visible_subpages) > 0:
                     page.menu_children = visible_subpages
                     page_list.append(page)
         return page_list
-    
+
     usergroups = request.user.groups.all()
     if getattr(request.user, "_ip_range_group_name", None):
         usergroups = Group.objects.filter(
--- myhpi/core/migrations/0011_auto_20220501_1642.py
+++ myhpi/core/migrations/0011_auto_20220501_1642.py
@@ -6,25 +6,25 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0010_auto_20211125_1225'),
+        ("core", "0010_auto_20211125_1225"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='informationpage',
-            name='is_public',
+            model_name="informationpage",
+            name="is_public",
             field=models.BooleanField(default=False),
             preserve_default=False,
         ),
         migrations.AddField(
-            model_name='minutes',
-            name='is_public',
+            model_name="minutes",
+            name="is_public",
             field=models.BooleanField(default=False),
             preserve_default=False,
         ),
         migrations.AddField(
-            model_name='minuteslist',
-            name='is_public',
+            model_name="minuteslist",
+            name="is_public",
             field=models.BooleanField(default=False),
             preserve_default=False,
         ),
