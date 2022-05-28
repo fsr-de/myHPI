9d3410336b1033b8d458900bb96a5a694fc39b25
DrEGZo <konacom@web.de>
2022-05-28 18:13:43 +0200

    Reduce page tree queries

---
+++ myhpi/core/templatetags/core_extras.py
@@ -0,0 +1,44 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag("menu_level.html")
+def build_menu_level(sub_pages_by_level_and_id, level):
+    return {"sub_pages_by_id": sub_pages_by_level_and_id.get(level, {}), "level": level}
+
+
+@register.filter
+def get(dict, key):
+    return dict[key]
+
+
+@register.filter
+def sub_menu_pages(page):
+    return page.menu_children
+
+
+@register.filter
+def all_sub_menu_pages_by_id(pages):
+    all_sub_pages_by_id = {}
+    for page in pages:
+        traverse_page_tree(page, [], all_sub_pages_by_id)
+    print(all_sub_pages_by_id)
+    return all_sub_pages_by_id
+
+
+def traverse_page_tree(curr_page, id_history, all_sub_pages):
+    id = format_id_history(id_history)
+    level_id = len(id_history)
+    same_level_sub_pages = all_sub_pages.get(level_id, {})
+    same_level_sub_pages[id] = same_level_sub_pages.get(id, []) + [curr_page]
+    all_sub_pages[level_id] = same_level_sub_pages
+    sub_pages = sub_menu_pages(curr_page)
+    for sub_page in sub_pages:
+        traverse_page_tree(sub_page, id_history + [curr_page.id], all_sub_pages)
+
+
+def format_id_history(id_history):
+    if not id_history:
+        return "root"
+    return str(id_history[-1])
