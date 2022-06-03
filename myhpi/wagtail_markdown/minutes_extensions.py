cd329aa881d44f488b56b4f5e52e28440706a8d9
frcroth <frcroth@gmail.com>
2022-06-03 12:00:00 +0200

    Support any character in enter, leave commands

--- myhpi/wagtail_markdown/minutes_extensions.py
+++ myhpi/wagtail_markdown/minutes_extensions.py
@@ -100,10 +100,13 @@ class EnterLeavePreprocessor(MinutesBasePreprocessor):
         super().__init__(*args, **kwargs)
         self.patterns = [
             (
-                r"\|enter\|\(([0-9:]+)\)\(([\w,\ ]+)\)(\((?P<mean_of_participation>.*?)\))?",
+                r"\|enter\|\(([0-9:]+)\)\(([^\)\(]+)\)(\((?P<mean_of_participation>.*?)\))?",
                 self.enterify,
             ),
-            (r"\|leave\|\(([0-9:]+)\)\(([\w,\ ]+)\)", self.leavify),
+            (
+                r"\|leave\|\(([0-9:]+)\)\(([^\)\(]+)\)",
+                self.leavify
+            ),
         ]
 
     def enter_or_leavify(self, match, event):
