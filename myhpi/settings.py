ea8295c2e473bfb06a4a470d50a5575f7230dae0
frcroth <frcroth@gmail.com>
2022-06-07 11:08:24 +0200

    Change BASE_URL to WAGTAILADMIN_BASE_URL

--- myhpi/settings.py
+++ myhpi/settings.py
@@ -196,7 +196,7 @@ WAGTAIL_SITE_NAME = "myHPI"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = "http://example.com"
+WAGTAILADMIN_BASE_URL = "http://example.com"
 
 ANONYMOUS_IP_RANGE_GROUPS = {"127.0.0.1": "Moderators"}
 
