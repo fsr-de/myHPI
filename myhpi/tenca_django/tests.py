59ab6161d24bf690bef14ed9d19eb3c248577329
frcroth <frcroth@gmail.com>
2022-06-15 22:36:45 +0200

    Migrate tenca to myhpi

---
+++ myhpi/tenca_django/tests.py
@@ -0,0 +1,30 @@
+import unittest
+
+from django.conf import settings
+from django.test import TestCase
+from tenca.tests import test_hash_storage
+
+from myhpi.tenca_django.models import DjangoModelHashStorage
+
+
+def skipUnlessListsEnabled():
+    if not settings.ENABLE_MAILING_LISTS:
+        return unittest.skip("Mailing lists not enabled in Django settings")
+
+    return lambda x: x
+
+
+@skipUnlessListsEnabled()
+class TencaSettingsLoaded(TestCase):
+    def setUp(self):
+        from myhpi.tenca_django.connection import connection
+
+        self.connection = connection
+
+    def testConnectionReceivedSettings(self):
+        self.assertEqual(settings.TENCA_TEST_LIST_DOMAIN, str(self.connection.domain))
+
+
+@skipUnlessListsEnabled()
+class DjangoModelHashStorageTest(TestCase, test_hash_storage.HiddenFromTestRunner.HashStorageTest):
+    StorageClass = DjangoModelHashStorage
