7da1e3cbfff900a4568224d252ceae875e609fba
Silvan Verhoeven <silvan.verhoeven@student.hpi.de>
2022-09-04 14:33:28 +0200

    fix: Ignore all folders starting with 'venv' in Git

--- myhpi/core/templatetags/core_extras.py
+++ myhpi/core/templatetags/core_extras.py
@@ -4,9 +4,11 @@ from myhpi.core.markdown.utils import render_markdown
 
 register = template.Library()
 
+
 @register.inclusion_tag("nav_level.html")
 def build_nav_level(level_pages, level=0, parent_id="root"):
     return {"level_pages": level_pages, "level": level, "parent_id": parent_id}
+
 
 @register.filter
 def nav_children(page):
