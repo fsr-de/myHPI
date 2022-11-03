1166c165b4e39ba88df3da0d1b7fdf7c943d5d8f
Julian B <jeriox@users.noreply.github.com>
2022-11-03 23:23:25 +0100

    remove unused code

--- myhpi/vouchers/templatetags/voucher_extras.py
+++ myhpi/vouchers/templatetags/voucher_extras.py
@@ -6,15 +6,5 @@ register = template.Library()
 
 
 @register.filter
-def first(voucher_allocation):
-    return voucher_allocation.vouchers.fisrt()
-
-
-@register.filter
-def other(voucher_allocation):
-    return voucher_allocation.vouchers.exclude(pk=voucher_allocation.vouchers.first().pk)
-
-
-@register.filter
 def is_angel(user):
     return models.is_angel(user)
