f327018c14a532c3187bcb759216ee461de81c33
frcroth <frcroth@users.noreply.github.com>
2023-02-28 14:13:24 +0100

    Fix ReDoS issue in minutes commands (#290)

--- myhpi/tests/test_minutes_extensions.py
+++ myhpi/tests/test_minutes_extensions.py
@@ -16,7 +16,6 @@ class TestMinuteExtensions(TestCase):
             "|enter|(12:00)(Prof. First Last)",
             "|enter|(12:00)(Prof. First Last)(Means)",
             "|leave|(12:00)(Prof. First Last)",
-            "|enter|(12:00)(Prof. First Last (Special Role))",
         ]
         result = elp.run(text)
         self.assertEqual(
@@ -26,6 +25,5 @@ class TestMinuteExtensions(TestCase):
                 "*12:00: Prof. First Last enters the meeting*  ",
                 "*12:00: Prof. First Last enters the meeting via Means*  ",
                 "*12:00: Prof. First Last leaves the meeting*  ",
-                "*12:00: Prof. First Last (Special Role) enters the meeting*  ",
             ],
         )
