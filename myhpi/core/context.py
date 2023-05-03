86441e54b83a02c1471ddafa5941456ef692c26b
frcroth <frcroth@gmail.com>
2023-05-03 22:56:53 +0200

    Improve performance of generating menu

--- myhpi/core/context.py
+++ myhpi/core/context.py
@@ -4,7 +4,6 @@ from wagtail.core.models import Site
 from .models import BasePage
 from .utils import get_user_groups
 
-
 def base_context(request):
     # How wagtail page trees work: https://www.accordbox.com/blog/how-to-create-and-manage-menus-in-wagtail/
 
@@ -19,13 +18,13 @@ def base_context(request):
     user_groups = get_user_groups(request.user)
 
     pages_visible_for_user = pages.filter(
-        Q(visible_for__in=user_groups) | Q(is_public=True)
+        (Q(visible_for__in=user_groups) | Q(is_public=True)) & (Q(show_in_menus=True) | Q(id=root_page.id))
     ).distinct()
 
     page_lookup = {}
 
     for page in pages_visible_for_user:
-        page_lookup[page.path] = pages_visible_for_user.child_of(page).in_menu()
+        page_lookup[page.path] = pages_visible_for_user.child_of(page)
 
     return {
         "root_page": root_page,
