cc768a27e4971a998293b8b4eb7daf7eeeb9b0a8
frcroth <frcroth@gmail.com>
2023-10-18 20:41:47 +0200

    Add security.txt

--- myhpi/urls.py
+++ myhpi/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     ),
     path("select2/", include("django_select2.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
+    path(".well-known/security.txt", RedirectView.as_view(url="/static/security.txt")),
 ]
 
 
