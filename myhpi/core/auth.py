7fe473b2d06789141f4bcdafc08908e8b7a37e19
Julian B <jeriox@users.noreply.github.com>
2023-11-09 16:08:51 +0100

    override sub from claims

--- myhpi/core/auth.py
+++ myhpi/core/auth.py
@@ -7,6 +7,9 @@ def mail_replacement(email):
 
 
 class MyHPIOIDCAB(OIDCAuthenticationBackend):
+    def _update_groups(self, user, claims):
+        return
+
     def create_user(self, claims):
         email = mail_replacement(claims.get("email"))
         first_name = claims.get("given_name", "")
@@ -16,24 +19,14 @@ class MyHPIOIDCAB(OIDCAuthenticationBackend):
         user = self.UserModel.objects.create_user(
             username, email=email, first_name=first_name, last_name=last_name
         )
-
-        try:
-            hpi = Group.objects.get(name="HPI")
-            user.groups.add(hpi)
-        except Group.DoesNotExist:
-            pass
-
-        if "student" in email.split("@")[1]:
-            try:
-                student = Group.objects.get(name="Student")
-                user.groups.add(student)
-            except Group.DoesNotExist:
-                pass
-
+        self._update_groups(user, claims)
         return user
 
     def update_user(self, user, claims):
-        user.email = mail_replacement(claims.get("email"))
+        user.email = claims.get("email")
+        user.first_name = claims.get("given_name", "")
+        user.last_name = claims.get("family_name", "")
+        self._update_groups(user, claims)
         user.save()
 
         return user
@@ -43,4 +36,7 @@ class MyHPIOIDCAB(OIDCAuthenticationBackend):
         username = claims.get("sub")
         if not username:
             return self.UserModel.objects.none()
-        return self.UserModel.objects.filter(username__iexact=username)
+        users = self.UserModel.objects.filter(username__iexact=username)
+        if not users.exists():
+            users = self.UserModel.objects.filter(email__iexact=mail_replacement(claims.get("email")))
+        return users
