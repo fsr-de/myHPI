e579593d88880223271cab5535045fcf156c9013
Julian B <jeriox@users.noreply.github.com>
2024-02-13 22:21:51 +0100

    fix test flow

--- myhpi/tests/settings.py
+++ myhpi/tests/settings.py
@@ -1,8 +1,5 @@
 from myhpi.settings import *  # NOQA, otherwise it will be removed by autoflake in the pre-commit hook
 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
-WAGTAIL_APPEND_SLASH = False
-
 METRICS_API_KEY = "TEST_KEY"
-
 LANGUAGE_CODE = "en"
