267f53d9d61a8e97d1c978a597a8c5a6cdf2eeb0
frcroth <frcroth@gmail.com>
2024-02-13 22:39:48 +0100

    Rename stuff

--- myhpi/core/templatetags/core_extras.py
+++ myhpi/core/templatetags/core_extras.py
@@ -53,11 +53,11 @@ def tag_external_links(content):
     return template.render(Context())
 
 
-@register.filter(name="touchify_abbrevations")
-def touchify_abbrevations(content):
-    """Takes the content of a website and inserts external link icons after every external link."""
-    external_links = re.finditer("<abbr[^>]*>[^<]*", content)
-    for link in reversed(list(external_links)):
+@register.filter(name="touchify_abbreviations")
+def touchify_abbreviations(content):
+    abbreviations = re.finditer("<abbr[^>]*>[^<]*", content)
+    print(list(abbreviations))
+    for link in reversed(list(abbreviations)):
         content = content[: link.start() + 5] + " tabindex=0" + content[link.start() + 5 :]
     template = Template("{% load bootstrap_icons %}" + content)
     return template.render(Context())
