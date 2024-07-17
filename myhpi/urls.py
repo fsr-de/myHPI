81b31a77ced89c90e36c8718c2a7a3a44d4472f1
frcroth <frcroth@users.noreply.github.com>
2024-07-17 09:05:55 +0200

    Set language cookie when selecting language (#577)

--- myhpi/urls.py
+++ myhpi/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
         ".well-known/security.txt",
         RedirectView.as_view(url=os.path.join(settings.STATIC_URL, "security.txt")),
     ),
+    path("i18n/", include("django.conf.urls.i18n")),
     path("", include("myhpi.core.urls")),
 ]
 
